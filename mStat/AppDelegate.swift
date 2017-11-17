fc18da6ceea7e87caa12b2364343554548441852
Venj <ersaclarke@gmail.com>
2017-11-17 14:09:21 +0800

    Initial Commit

---
+++ mStat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mStat
+//
+//  Created by venj on 2017/11/17.
+//  Copyright © 2017年 venj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mStat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mStat
+//
+//  Created by venj on 2017/11/17.
+//  Copyright © 2017年 venj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
